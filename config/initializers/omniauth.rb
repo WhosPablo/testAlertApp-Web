55a6db4fd821110285eb95386ee447a0c9fa2945
Pablo Arango <pabloaran17@gmail.com>
2015-11-21 18:44:16 -0500

    typo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :windowslive, ENV['WINDOWS_LIVE_KEY'],   ENV['WINDOWS_LIVEg_SECRET']
+  provider :windowslive, ENV['WINDOWS_LIVE_KEY'],   ENV['WINDOWS_LIVE_SECRET']
 
 end
