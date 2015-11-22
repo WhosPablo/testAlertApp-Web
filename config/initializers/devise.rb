a5ad897bd54ab67bde97065c46556268b8397927
Pablo Arango <pabloaran17@gmail.com>
2015-11-22 15:21:58 -0500

    Deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,7 +236,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :windowslive, ENV['WINDOWSLIVE_CLIENT_ID'], ENV['WINDOWSLIVE_SECRET'], scope: 'wl.basic,wl.emails'
+  config.omniauth :windowslive, ENV['WINDOWSLIVE_CLIENT_ID'], ENV['WINDOWSLIVE_SECRET'], scope: 'wl.basic,wl.emails,wl.photos'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'], scope: 'email,profile'
 
   # ==> Warden configuration
