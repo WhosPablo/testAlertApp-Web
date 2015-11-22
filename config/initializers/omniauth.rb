258fedc96b5e3d12c924257469cf8a9ab2dbc9eb
Pablo Arango <pabloaran17@gmail.com>
2015-11-22 01:58:59 -0500

    Setup

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-  puts ENV['WINDOWSLIVE_CLIENT_ID'], ENV['WINDOWSLIVE_SECRET']
-
-  provider :windowslive, ENV['WINDOWSLIVE_CLIENT_ID'],   ENV['WINDOWSLIVE_SECRET']
-
-end
