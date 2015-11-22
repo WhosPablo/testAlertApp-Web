56d55ac33a038ed29d29d7bf6d156a61971006f8
Pablo Arango <pabloaran17@gmail.com>
2015-11-22 13:54:28 -0500

    Deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  OmniAuth.config.full_host = "http://ippd-test-app.herokuapp.com"
+  #OmniAuth.config.full_host = "http://ippd-test-app.herokuapp.com"
 end
